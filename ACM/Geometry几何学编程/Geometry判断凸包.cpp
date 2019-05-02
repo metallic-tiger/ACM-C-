d36290d50028b1b625ba030bb5925d456c86eefa
matallic-tiger <511032766@qq.com>
2019-05-02 23:11:53 +0800

    提交

---
+++ ACM/Geometry几何学编程/Geometry判断凸包.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+
+#define MAXSize 50
+
+using namespace std;
+
+struct Point {
+float x,y;
+};
+
+struct PointSet{
+    Point P[MAXSize];
+    int   lenth;
+};
+
+/*
+进行矩阵运算
+计算两点的 相对于基础点的向量内积
+利用矩阵运算可知
+|A.x-Base.x |A.y-Base.y|=內积（带正负的面积）
+|B.x-Base.x |B.y-Base.y|
+=(A.x-Base.x)*(B.y-Base.y)-(B.x-Base.x)*(A.y-Base.y)
+
+根据面积的正负大小确定A点相对于B点是否在基础点的前方（顺时针参考）
+
+同时这个也是根据极坐标排序的偏序关系原则
+计算面积的公式
+float mult(Point Base,Point A,Point B){
+    return (A.x-Base.x)*(B.y-Base.y)-(B.x-Base.x)*(A.y-Base.y);
+}
+*/
+bool mult(Point Base,Point A,Point B){
+    return (A.x-Base.x)*(B.y-Base.y)>=(B.x-Base.x)*(A.y-Base.y);
+}
+
+
+PointSet GetTop(PointSet All){
+PointSet WALL;
+WALL.lenth=0;
+//排除特殊情况 没有点，单点 ，双点，三点（一平面）
+    if(All.lenth==0||All.lenth==1||All.lenth==2||All.lenth==3)
+    {
+        int i=0;
+        while(i!=All.lenth+1)
+        WALL.P[i]=All.P[i];
+        WALL.lenth=All.lenth;
+        return WALL;
+    }
+
+//找到处于最靠近x轴负无穷的点并删除这个点
+int Max=0;
+int Min=0;
+    for(int i=0;i<All.lenth;i++){
+        if(All.P[i].x>All.P[Max].x)
+            Max=i;
+        }
+WALL.P[0]=All.P[Max];
+WALL.lenth+=1;
+
+    for(int i=Max;i<All.lenth-1;i++){
+        All.P[i]=All.P[i+1];
+        }
+All.lenth--;
+
+
+/*
+进行相对于 基准点进行极角排序
+此处使用了快速排序加快的速度
+*/
+    for(int i=0;i<All.lenth/2;i++){
+        for(int j=i;j<All.lenth-i;j++)
+            {
+            if(mult(WALL.P[0],All.P[i],All.P[Max]))
+                Max=i;
+            if(!mult(WALL.P[0],All.P[i],All.P[Min]))
+                Min=i;
+            }
+    All.P[i]=All.P[Max];
+    All.P[All.lenth-1-i]=All.P[Min];
+    }
+
+/*
+开始进行连线啦
+参考了
+https://blog.csdn.net/zhouzi2018/article/details/82178412
+的方法
+*/
+    for(int i=0;i<All.lenth-1;i++){
+        if(mult(WALL.P[WALL.lenth-1],All.P[i],All.P[i+1]))
+            {
+                WALL.P[WALL.lenth]=All.P[i];
+                WALL.lenth++;
+            }
+        else{
+             WALL.P[WALL.lenth]=All.P[i];
+        }
+    }
+return WALL;
+}
+
+int main(){
+    PointSet All,Wall;
+    int i; 
+
+    All.lenth=Wall.lenth=0;
+
+    cout<<"输入点的个数"<<endl;
+    cin>>i;
+    All.lenth=i;
+    while(i){
+    i--;
+    cin>>All.P[i-1].x>>All.P[i-1].y;
+    }
+    Wall=GetTop(All);
+    for(int i=0;i<Wall.lenth;i++)
+    cout<<Wall.P[i].x<<" "<<Wall.P[i].y<<endl;
+    
+    return 0;
+}
